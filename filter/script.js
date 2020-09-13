094a2091ab0dfbc82bde231117d1f8e422cdf6c4
Natalie Smirnova <natalie.smirnowa@gmail.com>
2020-09-13 12:28:27 +0300

    add files

---
+++ filter/script.js
@@ -0,0 +1,60 @@
+var Filter = React.createClass({
+
+  dispayName: 'Filter',
+
+  getDefaultProps: function() {
+      return { 
+          fruits: ["ПУСТО"]
+      }
+  },
+  
+  propTypes: {
+    fruits: React.PropTypes.array.isRequired,
+  },
+
+
+  getInitialState: function() {
+    return {
+      input: "",
+      checked: false,
+      values: this.props.fruits.join('\n')
+    };
+  },
+
+
+ updateFruits: function() {
+   let fruits = this.props.fruits.filter(el => el.toLowerCase().includes(this.state.input.toLowerCase()));
+   if(this.state.checked){
+     fruits = fruits.sort();
+   }
+   this.setState({values: fruits.join('\n')});
+ },
+  
+
+ buttonClickHandler: function() {
+   onClick: this.setState({input: ""}, ()=> this.updateFruits());
+ },
+
+ checkboxSelected: function(event) {
+  this.setState({checked: event.target.checked}, ()=> this.updateFruits());
+  },
+
+ textChanged: function(event) {
+  this.setState({input: event.target.value}, ()=> this.updateFruits())
+ },
+
+  render: function() {
+  
+      return React.DOM.div(null,
+          React.DOM.label({className:'Block'},
+            React.DOM.input({
+              type:'checkbox', onClick: this.checkboxSelected
+            }),
+            React.DOM.input({type:'text', value:this.state.input, onInput: this.textChanged}),
+            React.DOM.button({onClick: this.buttonClickHandler}, "Сброс"),
+            React.DOM.textarea({value: this.state.values}),
+            )
+          )     
+  } 
+
+})
